1ece6b58efeaf4e8a5197968b8d05367681acc80
Christian <cvsejersen@gmail.com>
2023-12-29 17:27:07 +0100

    added brew.sh script

---
+++ dev/scripts/brew.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+brew install neovim
+brew install tmux
+brew install zoxide
+brew install git
+brew install lazygit
+brew install pyenv
+brew install qmk
+brew install skhd
+brew install yabai
+
