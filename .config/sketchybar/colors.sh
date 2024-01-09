979d15b7c815074d16cc97132ecc9c74afdae181
Christian <cvsejersen@gmail.com>
2024-01-09 20:48:48 +0100

    added sketchybar

---
+++ .config/sketchybar/colors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export WHITE=0xffffffff
+
+# -- Gray Scheme --
+export BAR_COLOR=0x25000000
+export ITEM_BG_COLOR=0x30000000
+export ACCENT_COLOR=0x85ffffff
+
+# -- Blue Scheme --- 
+# export BAR_COLOR=0xff021254
+# export ITEM_BG_COLOR=0xff093aa8
+# export ACCENT_COLOR=0xff15bdf9
