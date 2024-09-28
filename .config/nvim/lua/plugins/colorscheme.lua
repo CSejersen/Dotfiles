29d45a6055893ae89a5deb26375bc313ef23aeca
Christian <cvsejersen@gmail.com>
2024-09-28 14:43:51 +0200

    back to basics

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,37 @@
 return {
-	-- "navarasu/onedark.nvim", priorrity = 1000,
-	-- "catppuccin/nvim",
-	"yorumicolors/yorumi.nvim",
-	name = "yorumi",
+	"rose-pine/nvim",
+	-- "folke/tokyonight.nvim",
+	name = "rose-pine",
+	lazy = false,
 	priority = 1000,
+	opts = {},
+
 	config = function()
-		vim.cmd.colorscheme("yorumi")
+		-- Set the colorscheme
+		vim.cmd.colorscheme("rose-pine")
+
+		-- Make general elements transparent
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+		vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+		vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+		vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TabLineSel", { bg = "none" })
+
+		-- Make Telescope elements transparent
+		vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
 	end,
 }
--- .wezterm.lua
+++ .wezterm.lua
@@ -9,25 +9,11 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 15
 
+config.color_scheme = "Adventure"
+
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
-
-config.window_background_opacity = 1
-
-config.colors = {
-	foreground = "#BDBFCB",
-	background = "#060914",
-	cursor_bg = "#C6DFEC",
-	cursor_border = "#C6DFEC",
-	cursor_fg = "#1D202B",
-	selection_bg = "#C6DFEC",
-	selection_fg = "#1D202B",
-	ansi = { "#0C0F1A", "#F05C60", "#80AA6E", "#BA9A5E", "#597BC0", "#B4647F", "#A188C3", "#7AA8A7" },
-	brights = { "#656771", "#F47171", "#9CB67D", "#D6B476", "#788AD3", "#DA72A2", "#958EBE", "#85C7B8" },
-}
-
--- config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
 return config
